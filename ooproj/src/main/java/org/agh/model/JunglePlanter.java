ee7fd44b2df9998b5a955ad3f18230fe4dd0d639
Ant-Mas <pl.antos.m@gmail.com>
2024-12-28 14:34:10 +0100

    JunglePlanter with its own logic

--- ooproj/src/main/java/org/agh/model/JunglePlanter.java
+++ ooproj/src/main/java/org/agh/model/JunglePlanter.java
@@ -8,13 +8,14 @@ import java.util.*;
 
 public class JunglePlanter implements Planter{
 
+    private final static int BIGENERGYMULTIPLIER = 3;
+
     private HashMap<Vector2d, Plant> plants = new HashMap<>();
 
     //private Fields fields;
     private final Set<Vector2d> preferredFields;
     private final Set<Vector2d> regularFields;
-    private final List<Vector2d> preferredFieldsAvailable;
-    private final List<Vector2d> regularFieldsAvailable;
+    private final List<Vector2d> fieldsAvailable;
     private final int plantEnergy;
 
     private static final Random random = new Random();
@@ -24,8 +25,8 @@ public class JunglePlanter implements Planter{
         this.plantEnergy = plantEnergy;
         this.preferredFields = fields.getPreferredFields();
         this.regularFields = fields.getRegularFields();
-        this.preferredFieldsAvailable = new ArrayList<>(preferredFields);
-        this.regularFieldsAvailable = new ArrayList<>(regularFields);
+        this.fieldsAvailable = new ArrayList<>(preferredFields);
+        this.fieldsAvailable.addAll(regularFields);
     }
     public JunglePlanter(int width, int height, int plantEnergy, int startingPlants) {
         this(width, height, plantEnergy);
@@ -34,67 +35,73 @@ public class JunglePlanter implements Planter{
 
     @Override
     public void generatePlants(int plantAmount) {
-        int preferredFieldsCount = preferredFieldsAvailable.size();
-        int regularFieldsCount = regularFieldsAvailable.size();
-
+        int selectedIndex = 0;
+        Vector2d selectedPosition = null;
         for (int i = 0; i < plantAmount; i++) {
-            double chance = random.nextDouble();
-            //there is 80% chance that plant grows on a preferred field, or if the regular fields are full and the
-            // preferred are not the plant must grow on the preferred field.
-            if ((chance >= 0.2 && preferredFieldsCount > 0) || (regularFieldsCount == 0 && preferredFieldsCount > 0)) {
-                int ind = random.nextInt(preferredFieldsCount);
-                Vector2d field = preferredFieldsAvailable.get(ind);
-                plants.put(field, new Plant(field, plantEnergy));
-                preferredFieldsAvailable.remove(ind);
-                preferredFieldsCount--;
+            if(fieldsAvailable.isEmpty()) break;
+            selectedIndex = random.nextInt(fieldsAvailable.size());
+            selectedPosition = fieldsAvailable.get(selectedIndex);
+            if(!tryBigPlacing(selectedPosition)) {
+
+                plants.put(selectedPosition, new Plant(selectedPosition, plantEnergy));
             }
-            //there is 20% chance that plant grows on a regular field
-            //if all preferred fields are already taken by plants, then for map to have exactly starting amount of plants
-            //we have to distribute remaining plants onto regular field, hence second condition
-            else if ( (chance < 0.2 && regularFieldsCount > 0) || (preferredFieldsCount == 0 && regularFieldsCount > 0)) {
-                int ind = random.nextInt(regularFieldsCount);
-                Vector2d field = regularFieldsAvailable.get(ind);
-                plants.put(field, new Plant(field, plantEnergy));
-                regularFieldsAvailable.remove(ind);
-                regularFieldsCount--;
-            }
+            fieldsAvailable.set(selectedIndex, fieldsAvailable.getLast());
+            fieldsAvailable.removeLast();
         }
+    }
+
+    private boolean tryBigPlacing(Vector2d lowerLeft){
+        List<Vector2d> neighbours = List.of(lowerLeft.add(new Vector2d(0,1)),
+                                            lowerLeft.add(new Vector2d(1,0)),
+                                            lowerLeft.add(new Vector2d(1,1)));
+        if(     isPreferred(lowerLeft) &&
+                isPreferred(neighbours.getLast()) &&
+                !plants.containsKey(neighbours.getFirst()) &&
+                !plants.containsKey(neighbours.getLast()) &&
+                !plants.containsKey(neighbours.get(1))
+        ) {
+            BigPlant bigPlant = new BigPlant(lowerLeft, BIGENERGYMULTIPLIER*plantEnergy);
+            plants.put(lowerLeft, bigPlant);
+            for(Vector2d position: neighbours){
+                plants.put(position, bigPlant);
+            }
+            return true;
+        }
+        return false;
     }
 
     public Plant removePlant(Vector2d position) {
         if (this.plants.containsKey(position)) {
             Plant plant = this.plants.get(position);
 
-            if (isPreferred(position)) {
-                this.addToPreferredFieldsAvailable(position);
+            if(plant instanceof BigPlant) {
+                List<Vector2d> positions = List.of(plant.position,
+                        plant.position.add(new Vector2d(0,1)),
+                        plant.position.add(new Vector2d(1,0)),
+                        plant.position.add(new Vector2d(1,1)));
+                for(Vector2d position2: positions){
+                    this.addToFieldsAvailable(position2);
+                    plants.remove(position2);
+                }
             }
             else {
-                this.addToRegularFieldsAvailable(position);
+                this.addToFieldsAvailable(position);
+                this.plants.remove(position);
             }
-
-            this.plants.remove(position);
             return plant;
         }
         return null;
     }
 
     //handling regular fields availability
-    private void addToRegularFieldsAvailable(Vector2d position) {
-        regularFieldsAvailable.add(position);
+    private void addToFieldsAvailable(Vector2d position) {
+        fieldsAvailable.add(position);
     }
 
-    private void removeFromRegularFieldsAvailable(Vector2d position) {
-        regularFieldsAvailable.remove(position);
+    private void removeFromFieldsAvailable(Vector2d position) {
+        fieldsAvailable.remove(position);
     }
 
-    //handling preferred fields availability
-    private void addToPreferredFieldsAvailable(Vector2d position) {
-        preferredFieldsAvailable.add(position);
-    }
-
-    private void removeFromPreferredFieldsAvailable(Vector2d position) {
-        preferredFieldsAvailable.remove(position);
-    }
 
     @Override
     public boolean isPreferred(Vector2d position) {
@@ -120,13 +127,5 @@ public class JunglePlanter implements Planter{
 
     public Set<Vector2d> getRegularFields() {
         return regularFields;
-    }
-
-    public List<Vector2d> getPreferredFieldsAvailable() {
-        return preferredFieldsAvailable;
-    }
-
-    public List<Vector2d> getRegularFieldsAvailable() {
-        return regularFieldsAvailable;
     }
 }
