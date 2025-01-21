4f714e7f2029c6571a666a23c603745dc76379c5
NCCMNT <chmielewski2003@gmail.com>
2025-01-21 01:52:51 +0100

    minor fixes, added map dimensions restrictions

--- ooproj/src/main/java/org/agh/controller/StartingController.java
+++ ooproj/src/main/java/org/agh/controller/StartingController.java
@@ -156,8 +156,8 @@ public class StartingController implements Controller {
             Stage simulationStage = new Stage();
             simulationStage.setScene(simulationScene);
             simulationStage.setTitle("Simulation");
-            simulationStage.setMinHeight(800);
-            simulationStage.setMinWidth(1400);
+            simulationStage.setMinHeight(850);
+            simulationStage.setMinWidth(1800);
 
             // handling closing window of simulation
             simulationStage.setOnCloseRequest(event -> simulationController.getSimulation().shutdown());
